9d0db83cbbb9a053e64d7b1f5131dd43bfd3ac35
Lisa Fredou <lisa.fredou@gmail.com>
2024-03-27 10:19:16 +0100

    creation fichier header/footer

--- wp-content/themes/nathalie-mota/functions.php
+++ wp-content/themes/nathalie-mota/functions.php
@@ -1,9 +1,7 @@
-<?php
-/**
-** activation theme
-**/
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-function theme_enqueue_styles() {
- wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+<?php 
 
-}
+// Ajouter la prise en charge des images mises en avant
+add_theme_support( 'post-thumbnails' );
+
+// Ajouter automatiquement le titre du site dans l'en-tête du site
+add_theme_support( 'title-tag' );
