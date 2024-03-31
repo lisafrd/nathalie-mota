c0853149dfb5f3f30c52cd6808f23c8c5ceaae41
Lisa Fredou <lisa.fredou@gmail.com>
2024-03-31 18:58:30 +0200

    Creation ACF photos dynamiques

--- wp-content/themes/nathalie-mota/front-page.php
+++ wp-content/themes/nathalie-mota/front-page.php
@@ -2,7 +2,7 @@
 
 <div class="front-container">
     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/nathalie-0-min.jpeg" alt="Image d'accueil" class="image-front">
-    <h1>PHOTOGRAPHE EVENT</h1>
+    <h1 class="front-title">PHOTOGRAPHE EVENT</h1>
 </div>
 
 <?php get_footer(); ?>
--- wp-content/themes/nathalie-mota/single.php
+++ wp-content/themes/nathalie-mota/single.php
@@ -8,6 +8,28 @@
                 while (have_posts()) :
                     the_post();
                     get_template_part('template-parts/content-single');
+
+                    // Vérifier si le type de publication est "photo"
+                    if (get_post_type() == 'photos') :
+
+                        // Afficher les champs personnalisés ACF 
+                ?>
+                        <ul class="ACF-container">
+                            <div class="content-ACF-container">
+                                <li class="li-ACF-photo">RÉFÉRENCE : <?php the_field('reference'); ?></li>
+                                <li class="li-ACF-photo">CATÉGORIE : <?php the_field('categorie'); ?></li>
+                                <li class="li-ACF-photo">FORMAT : <?php the_field('format'); ?></li>
+                                <li class="li-ACF-photo">TYPE : <?php the_field('type'); ?></li>
+                                <li class="li-ACF-photo">ANNÉE : <?php the_field('annee'); ?></li>
+                            </div>
+                            <li class="img-ACF-container"><?php $image = get_field('photo'); ?>
+                                <img class="img-ACF" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
+                            </li>
+                        </ul>
+
+                        <div class="ACF-line"></div>
+                <?php
+                    endif;
                 endwhile;
                 ?>
             </div>
